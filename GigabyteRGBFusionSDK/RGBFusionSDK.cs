1a8487bc575fcb7fac9602709758c808ca594e13
Codie Collinge <c.j.collinge@live.co.uk>
2019-01-13 12:45:21 +0000

    Imports some more SDK functions into the wrapper library and utilizes them in the SampleApp to identify motherboard lighting zones by looping through and flashing each zone white. (Looks like I only have 2 zones on my MB)

--- GigabyteRGBFusionSDK/RGBFusionSDK.cs
+++ GigabyteRGBFusionSDK/RGBFusionSDK.cs
@@ -9,8 +9,22 @@ namespace GigabyteRGBFusionSDKWrapper
 {
     public class RGBFusionSDK
     {
+        public struct LedSetting
+        {
+            public byte m_Reserve0;
+            public byte m_LedMode;
+            public byte m_MaxBrightness;
+            public byte m_MinBrightness;
+            public uint m_Colour;
+            public ushort m_Time0;
+            public ushort m_Time1;
+            public ushort m_Time2;
+            public byte m_CtrlVal0;
+            public byte m_CtrlVal1;
+        }
+
         [DllImport("GLedApi ", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
-        private static extern ulong dllexp_GetSdkVersion(StringBuilder lpReturnBuf, int bufLen);
+        private static extern uint dllexp_GetSdkVersion(StringBuilder lpReturnBuf, int bufLen);
         public static ulong GetSdkVersion(out String returnStr)
         {
             StringBuilder strBuilder = new StringBuilder(16);
@@ -18,5 +32,26 @@ namespace GigabyteRGBFusionSDKWrapper
             returnStr = strBuilder.ToString();
             return output;
         }
+
+        [DllImport("GLedApi ", EntryPoint = "dllexp_InitAPI", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern uint InitAPI();
+
+        [DllImport("GLedApi ", EntryPoint = "dllexp_GetMaxDivision", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern int GetMaxDivision();
+
+        [DllImport("GLedApi ", EntryPoint = "dllexp_GetLedLayout", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern uint GetLedLayout(byte[] array, int arraySize);
+
+        [DllImport("GLedApi ", EntryPoint = "dllexp_SetLedData", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern uint dllexp_SetLedData(LedSetting[] setting, int arraySize);
+        public static uint SetLedData(LedSetting[] settingData)
+        {
+            return dllexp_SetLedData(settingData, Marshal.SizeOf(settingData[0]) * settingData.Length);
+        }
+
+        [DllImport("GLedApi ", EntryPoint = "dllexp_Apply", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern uint Apply(int applyBit);
+
+
     }
 }
--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -13,9 +13,56 @@ namespace SampleApp
             string rgbFusionSDKVer = "";
             RGBFusionSDK.GetSdkVersion(out rgbFusionSDKVer);
             Console.WriteLine("Gigabyte Fusion RGB SDK Version " + rgbFusionSDKVer + " Detected.");
+            Console.WriteLine("Initializing...");
 
-            Console.WriteLine("Press Any Key to Exit...");
-            Console.ReadKey();
+            uint result = RGBFusionSDK.InitAPI();
+
+            if (result != 0u) {
+                Console.WriteLine("Failed to Intialize RGB Fusion SDK");
+                return;
+            }
+
+            Console.WriteLine("Successfully Initialized RGB Fusion SDK");
+
+            int maxDivision = RGBFusionSDK.GetMaxDivision();
+            Console.WriteLine("Max LED zones supported: " + maxDivision);
+
+            byte[] ledLayout = new byte[maxDivision];
+            result = RGBFusionSDK.GetLedLayout(ledLayout, maxDivision);
+
+            RGBFusionSDK.LedSetting[] nullAllSettings = new RGBFusionSDK.LedSetting[maxDivision];
+            for (var i = 0; i < nullAllSettings.Length; i++)
+            {
+                nullAllSettings[i].m_LedMode = 0;
+            }
+
+            RGBFusionSDK.LedSetting[] ledSettings = new RGBFusionSDK.LedSetting[maxDivision];
+
+            for(var i = 0; i < ledSettings.Length; i++)
+            {
+                ledSettings[i].m_LedMode = 5;
+                ledSettings[i].m_MaxBrightness = 100;
+                ledSettings[i].m_MinBrightness = 0;
+                ledSettings[i].m_Colour = 0xFFFFFFFF;
+                ledSettings[i].m_Time0 = 500;
+                ledSettings[i].m_Time1 = 700;
+                ledSettings[i].m_Time2 = 700 * 50;
+                ledSettings[i].m_CtrlVal0 = 50;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("IDENTIFYING ZONES");
+            for (var i = 0; i < maxDivision; i++)
+            {
+                result = RGBFusionSDK.SetLedData(ledSettings);
+                int applyBit = (int)Math.Pow(2, i);
+                result = RGBFusionSDK.Apply(applyBit);
+                Console.WriteLine("Zone " + i + " by Flashing White.");
+                Console.WriteLine("Press ANY KEY to move to next zone.\n");
+                Console.ReadKey();
+                result = RGBFusionSDK.SetLedData(nullAllSettings);
+                result = RGBFusionSDK.Apply(-1);
+            }
         }
     }
 }
